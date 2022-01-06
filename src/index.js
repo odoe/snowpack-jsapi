052bf19aab364bbda76954e0a34c69d35d842eaf
Rene Rubalcava <rrubalcava@esri.com>
2022-01-06 13:42:24 -0800

    update packages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ArcGISMap from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
 import Search from '@arcgis/core/widgets/Search';
 import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+import Legend from '@arcgis/core/widgets/Legend';
 
 const map = new ArcGISMap({
   basemap: 'topo-vector'
@@ -10,7 +11,7 @@ const map = new ArcGISMap({
 const view = new MapView({
   container: "viewDiv",
   map: map,
-  zoom: 9,
+  zoom: 8,
   extent: {
     // autocasts as new Extent()
     xmin: -9177811,
@@ -22,8 +23,10 @@ const view = new MapView({
 });
 
 const search = new Search({ view });
-
 view.ui.add(search, 'top-right');
+
+const legend = new Legend({ view });
+view.ui.add(legend, 'bottom-right');
  
 const featureLayer = new FeatureLayer({
   url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/Landscape_Trees/FeatureServer/0",
